be2b6d0b4a0b68724c1a863a967a97ca9ef7bae4
maxwell3739 <maxwell@uvic.ca>
2018-03-15 17:02:25 +0800

    Rowan is the best

--- src/components/BudgetPage.js
+++ src/components/BudgetPage.js
@@ -8,9 +8,10 @@ import OutletCard from './OutletCard';
 
 var outletData = [{name: "Oven", usage: 50}, {name: "Stove", usage: 40 }, {name: "Light1", usage: 20 }, {name: "Light2", usage: 400 }, {name: "Microwave", usage: 100 } ]
 
-var budgetData = {
-  dailySpend: [{1: 50}, {2: 40}, {3: 10}, {4: 30}]
-}
+//Sorts Data from largest to smallest usage
+outletData.sort(function(a, b) {
+    return b.usage - a.usage;
+});
 
 class Budget extends React.Component {
  render() {
@@ -19,7 +20,6 @@ class Budget extends React.Component {
        <Grid>
          <Row className="show-grid">
            {outletData.map(outlet => {
-             console.log(outlet)
              return <OutletCard
                name = {outlet.name}
                usage = {outlet.usage}
